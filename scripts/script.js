5b1abf149c79142fe66f4282ffffb0f598d4cb30
rxeltyi <50542435+nefarixus@users.noreply.github.com>
2024-12-17 12:10:06 +0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+let menu = document.querySelector("header nav");
+let menuToggle = document.querySelector(".menu-toggle");
+
+menuToggle.addEventListener("click", function(e) {
+    e.preventDefault();
+    menu.classList.toggle("show-menu");
+    menuToggle.classList.toggle("active");
+});
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 480 && menu.classList.contains("show-menu")) {
+        menu.classList.remove("show-menu");
+        menuToggle.classList.remove("active");
+    }
+});
